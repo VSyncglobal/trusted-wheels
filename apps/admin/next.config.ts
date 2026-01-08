c81dc38bb72b024be1582c51b4fd8a31100c8bc3
Samwel Chege <kingsamtex@gmail.com>
2026-01-08 10:54:05 +0300

    unoptimized: true

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -2,14 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.r2.dev', // Allow Cloudflare R2 domains
+        hostname: '**.r2.dev', // Cloudflare R2
       },
       {
         protocol: 'https',
-        hostname: '**.amazonaws.com', // Keep AWS just in case
+        hostname: '**.amazonaws.com', // AWS S3
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Unsplash (For Hero Slider)
       }
     ],
   },
